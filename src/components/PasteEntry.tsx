d995edccf07691b3f21daeb7e121f66d5cec509b
Tavija N <tavijaja@gmail.com>
2021-08-23 20:10:39 +0300

    Created component layout and added expandable Read Mode

--- src/components/PasteEntry.tsx
+++ src/components/PasteEntry.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 interface PasteEntryProps {
     paste_id: number;
     title: string;
@@ -10,13 +12,23 @@ export function PasteEntry({
     title,
     content,
     time
- }: PasteEntryProps): JSX.Element {
+}: PasteEntryProps): JSX.Element {
+
+    const [readMore, setReadMore] = useState(false);
+    const extraContent = <div>
+        <p>{content}</p>
+    </div>
+    const linkName = readMore ? 'Read Less << ' : 'Read More >> '
+
 
     return (
-        <section className="individual-paste">
+        <div className="individual-paste">
             <h2>{title}</h2>
-            <p>{content}</p>
-            <p>Posted at: {time.slice(0,10)} {time.slice(11,16)}</p>
-        </section>
+            <div>
+                <a className="read-more-link" onClick={() => { setReadMore(!readMore) }}><p>{linkName}</p></a>
+                {readMore && extraContent}
+            </div>
+            <p>Posted at: {time.slice(0, 10)} {time.slice(11, 16)}</p>
+        </div>
     )
- }
+}
