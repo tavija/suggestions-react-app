61f373eea9eba1d2e40927d52f48af0c8e0f518d
Tavija N <tavijaja@gmail.com>
2021-08-25 16:49:24 +0300

    Changed expandable <a> link to a button to fix console errors

--- src/components/PasteEntry.tsx
+++ src/components/PasteEntry.tsx
@@ -4,7 +4,7 @@ interface PasteEntryProps {
     paste_id: number;
     title: string;
     content: string;
-    time: any;
+    time: string;
 }
 
 export function PasteEntry({
@@ -18,16 +18,14 @@ export function PasteEntry({
     const extraContent = <div>
         <p>{content}</p>
     </div>
-    const linkName = readMore ? 'Read Less << ' : 'Read More >> '
+    const linkName = readMore ? 'Read Less << ' : 'Read Content >> '
 
 
     return (
         <div className="individual-paste">
             <h2>{title}</h2>
-            <div>
-                <a className="read-more-link" onClick={() => { setReadMore(!readMore) }}><p>{linkName}</p></a>
-                {readMore && extraContent}
-            </div>
+            <button className="read-more-link" onClick={() => { setReadMore(!readMore) }}>{linkName}</button>
+            {readMore && extraContent}
             <p>Posted at: {time.slice(0, 10)} {time.slice(11, 16)}</p>
         </div>
     )
