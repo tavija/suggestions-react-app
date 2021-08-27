7272144dd721a1cf133d5c846cdd4a26f3b5958a
Tavija N <tavijaja@gmail.com>
2021-08-27 12:41:17 +0300

    Renamed files and functions

---
+++ src/components/PasteHistory.tsx
@@ -0,0 +1,22 @@
+import { Paste } from "../App";
+import { PasteEntry } from "./PasteEntry";
+
+interface PasteHistoryProps {
+    pastesList: Paste[];
+}
+
+export function pasteHistory(props: PasteHistoryProps): JSX.Element {
+    return (
+        <div className="paste-history flex-right">
+            <h2>Previous Pastes</h2>
+            <div className="scroll-bar">
+                {/* slice creates shallow copy, reverse reverses it*/}
+                {[...props.pastesList].reverse().map((paste: Paste) => (
+                    <PasteEntry
+                        key={paste.paste_id}
+                        {...paste}
+                    />))}
+            </div>
+        </div>
+    );
+}
