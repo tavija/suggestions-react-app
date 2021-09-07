c22edc1d690c9aacc7995786f07c7306683bd29b
Tavija N <tavijaja@gmail.com>
2021-09-07 22:17:08 +0300

    Updated textarea style and changed reversed suggestionsList copy

--- src/components/SuggestionsHistory.tsx
+++ src/components/SuggestionsHistory.tsx
@@ -9,20 +9,19 @@ interface ISuggestionsHistory {
 }
 
 export function SuggestionsHistory(props: ISuggestionsHistory): JSX.Element {
+  
   return (
     <div>
       <div className="suggestion-history">
-        {[...props.suggestionsList]
-          .reverse()
-          .map((suggestion: SuggestionProps) => (
-            <SuggestionEntry
-              key={suggestion.suggestion_id}
-              suggestion={suggestion}
-              handleVote={props.handleVote}
-              handleDelete={props.handleDelete}
-              username={props.username}
-            />
-          ))}
+        {props.suggestionsList.map((suggestion: SuggestionProps) => (
+          <SuggestionEntry
+            key={suggestion.suggestion_id}
+            suggestion={suggestion}
+            handleVote={props.handleVote}
+            handleDelete={props.handleDelete}
+            username={props.username}
+          />
+        ))}
         {props.suggestionsList.length === 0 && (
           <h3 className="center">
             Suggestion box is empty. Maybe you could submit one?
